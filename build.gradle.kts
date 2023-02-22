b1d30d490f8657ff665f8a8a8afc0b80280a0f3f
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-02-22 13:27:17 +0300

    Configure Maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,43 @@
-@file:Suppress("UnstableApiUsage")
+@file:Suppress(
+    "UnstableApiUsage",
+    "KDocMissingDocumentation",
+)
 
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.Named
+import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.internal.logging.text.StyledTextOutput
+import org.gradle.internal.logging.text.StyledTextOutput.Style.Failure
+import org.gradle.internal.logging.text.StyledTextOutput.Style.Success
+import org.gradle.internal.logging.text.StyledTextOutputFactory
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.support.serviceOf
+import org.gradle.kotlin.dsl.withType
+import org.gradle.plugins.signing.SigningExtension
 
 plugins {
     kotlin("multiplatform") version "1.8.10"
     eclipse
     `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "1.7.20"
 }
 
 group = "com.saveourtool"
 version = "1.0-SNAPSHOT"
+description = "A set of extensions to Okio"
 
 repositories {
     mavenCentral()
 }
 
 kotlin {
-    jvm {
-        withJava()
-    }
+    jvm()
     jvmToolchain(jdkVersion = 17)
 
     @Suppress(
@@ -73,15 +92,156 @@ tasks.withType<Test> {
     }
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "GitHub"
-            url = uri("https://maven.pkg.github.com/saveourtool/okio-extras")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+tasks.withType<AbstractPublishToMaven> {
+    dependsOn(tasks.withType<Sign>())
+}
+
+configurePublishing()
+
+fun Project.configurePublishing() {
+    configureGitHubPublishing()
+    configurePublications()
+    configureSigning()
+}
+
+fun Project.configureGitHubPublishing() =
+    publishing {
+        repositories {
+            maven {
+                name = "GitHub"
+                url = uri("https://maven.pkg.github.com/saveourtool/okio-extras")
+                credentials {
+                    username = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                    password = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
+
+fun Project.configurePublications() {
+    val dokkaJar = tasks.create<Jar>("dokkaJar") {
+        group = "documentation"
+        archiveClassifier.set("javadoc")
+        from(tasks.findByName("dokkaHtml"))
+    }
+
+    configure<PublishingExtension> {
+        publications.withType<MavenPublication>().configureEach {
+            this.artifact(dokkaJar)
+            this.pom {
+                val project = this@configurePublications
+
+                name.set(project.name)
+                description.set(project.description ?: project.name)
+                url.set("https://github.com/saveourtool/${project.name}")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("0x6675636b796f75676974687562")
+                        name.set("Andrey Shcheglov")
+                        email.set("shcheglov.av@phystech.edu")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/saveourtool/${project.name}")
+                    connection.set("scm:git:https://github.com/saveourtool/${project.name}.git")
+                    developerConnection.set("scm:git:git@github.com:saveourtool/${project.name}.git")
+                }
             }
         }
     }
 }
+
+/**
+ * Enables signing of the artifacts if the `signingKey` project property is set.
+ *
+ * Should be explicitly called after each custom `publishing {}` section.
+ */
+fun Project.configureSigning() {
+    System.getenv("GPG_SEC")?.let {
+        extra.set("signingKey", it)
+    }
+    System.getenv("GPG_PASSWORD")?.let {
+        extra.set("signingPassword", it)
+    }
+
+    if (hasProperty("signingKey")) {
+        /*
+         * GitHub Actions.
+         */
+        configureSigningCommon {
+            useInMemoryPgpKeys(property("signingKey") as String?, findProperty("signingPassword") as String?)
+        }
+    } else if (
+        hasProperties(
+            "signing.keyId",
+            "signing.password",
+            "signing.secretKeyRingFile",
+        )
+    ) {
+        /*-
+         * Pure-Java signing mechanism via `org.bouncycastle.bcpg`.
+         *
+         * Requires an 8-digit (short form) PGP key id and a present `~/.gnupg/secring.gpg`
+         * (for gpg 2.1, run
+         * `gpg --keyring secring.gpg --export-secret-keys >~/.gnupg/secring.gpg`
+         * to generate one).
+         */
+        configureSigningCommon()
+    } else if (hasProperty("signing.gnupg.keyName")) {
+        /*-
+         * Use an external `gpg` executable.
+         *
+         * On Windows, you may need to additionally specify the path to `gpg` via
+         * `signing.gnupg.executable`.
+         */
+        configureSigningCommon {
+            useGpgCmd()
+        }
+    }
+}
+
+/**
+ * @param useKeys the block which configures the PGP keys. Use either
+ *   [SigningExtension.useInMemoryPgpKeys], [SigningExtension.useGpgCmd], or an
+ *   empty lambda.
+ * @see SigningExtension.useInMemoryPgpKeys
+ * @see SigningExtension.useGpgCmd
+ */
+@Suppress(
+    "MaxLineLength",
+    "SpreadOperator",
+)
+fun Project.configureSigningCommon(useKeys: SigningExtension.() -> Unit = {}) {
+    configure<SigningExtension> {
+        useKeys()
+        val publications = extensions.getByType<PublishingExtension>().publications
+        val publicationCount = publications.size
+        val message = "The following $publicationCount publication(s) are getting signed: ${publications.map(Named::getName)}"
+        val style = when (publicationCount) {
+            0 -> Failure
+            else -> Success
+        }
+        styledOut(logCategory = "signing").style(style).println(message)
+        sign(*publications.toTypedArray())
+    }
+}
+
+fun Project.styledOut(logCategory: String): StyledTextOutput =
+    serviceOf<StyledTextOutputFactory>().create(logCategory)
+
+/**
+ * Determines if this project has all the given properties.
+ *
+ * @param propertyNames the names of the properties to locate.
+ * @return `true` if this project has all the given properties, `false` otherwise.
+ * @see Project.hasProperty
+ */
+fun Project.hasProperties(vararg propertyNames: String): Boolean =
+    propertyNames.asSequence().all(this::hasProperty)
