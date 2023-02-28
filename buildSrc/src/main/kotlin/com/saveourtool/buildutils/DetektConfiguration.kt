7638e0e8479f2fbed5318eeefb62c878327f5549
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-02-28 16:23:36 +0300

    Introduce the File.div extension

--- buildSrc/src/main/kotlin/com/saveourtool/buildutils/DetektConfiguration.kt
+++ buildSrc/src/main/kotlin/com/saveourtool/buildutils/DetektConfiguration.kt
@@ -8,24 +8,25 @@ import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
+import java.io.File
 
 fun Project.configureDetekt() {
     tasks.withType<Detekt> {
         parallel = true
         autoCorrect = hasProperty("detektAutoCorrect")
-        config.setFrom(file(projectDir.resolve("config").resolve("detekt").resolve("detekt.yml")))
+        config.setFrom(file(projectDir / "config" / "detekt" / "detekt.yml"))
         include("**/*.kt")
         reports {
-            val reportDir = buildDir.resolve("reports").resolve("detekt")
+            val reportDir = buildDir / "reports" / "detekt"
 
             sarif {
                 required.set(true)
-                outputLocation.set(file(reportDir.resolve("$name.sarif")))
+                outputLocation.set(file(reportDir / "$name.sarif"))
             }
 
             html {
                 required.set(true)
-                outputLocation.set(file(reportDir.resolve("$name.html")))
+                outputLocation.set(file(reportDir / "$name.html"))
             }
 
             sequenceOf(xml, txt, md).forEach { report ->
@@ -36,22 +37,22 @@ fun Project.configureDetekt() {
 
     tasks.register<Detekt>("detektCommonMain") {
         dependsOn(tasks.named<KotlinCompile>("compileKotlinJvm"))
-        source = fileTree(projectDir.resolve("src").resolve("commonMain"))
+        source = fileTree(projectDir / "src" / "commonMain")
     }
 
     tasks.register<Detekt>("detektCommonTest") {
         dependsOn(tasks.named<KotlinCompile>("compileTestKotlinJvm"))
-        source = fileTree(projectDir.resolve("src").resolve("commonTest"))
+        source = fileTree(projectDir / "src" / "commonTest")
     }
 
     tasks.named<Detekt>("detektJvmMain") {
         dependsOn(tasks.named<KotlinCompile>("compileKotlinJvm"))
-        source = fileTree(projectDir.resolve("src").resolve("jvmMain"))
+        source = fileTree(projectDir / "src" / "jvmMain")
     }
 
     tasks.named<Detekt>("detektJvmTest") {
         dependsOn(tasks.named<KotlinCompile>("compileTestKotlinJvm"))
-        source = fileTree(projectDir.resolve("src").resolve("jvmTest"))
+        source = fileTree(projectDir / "src" / "jvmTest")
     }
 
     tasks.register<Detekt>("detektNativeMain") {
@@ -60,7 +61,7 @@ fun Project.configureDetekt() {
             tasks.named<KotlinNativeCompile>("compileKotlinLinuxX64"),
             tasks.named<KotlinNativeCompile>("compileKotlinMacosX64"),
         )
-        source = fileTree(projectDir.resolve("src").resolve("nativeMain"))
+        source = fileTree(projectDir / "src" / "nativeMain")
     }
 
     tasks.register<Detekt>("detektNativeTest") {
@@ -69,7 +70,7 @@ fun Project.configureDetekt() {
             tasks.named<KotlinNativeCompile>("compileTestKotlinLinuxX64"),
             tasks.named<KotlinNativeCompile>("compileTestKotlinMacosX64"),
         )
-        source = fileTree(projectDir.resolve("src").resolve("nativeTest"))
+        source = fileTree(projectDir / "src" / "nativeTest")
     }
 
     tasks.register<DefaultTask>("detektAll") {
@@ -83,3 +84,6 @@ fun Project.configureDetekt() {
         )
     }
 }
+
+private operator fun File.div(relative: String): File =
+    resolve(relative)
